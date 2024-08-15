dae93cf05ee625e70261f211324811345ee81032
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-08-15 17:10:09 +0700

    2.7

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -41,7 +41,7 @@ onMounted(() => {
 
 <template>
   <h1>Events For Good</h1>
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <EventDetail v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
@@ -64,14 +64,9 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
-  width: 290px;
+  width: 290px
 }
 .pagination a {
   flex: 1;
